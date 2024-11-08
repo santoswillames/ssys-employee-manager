154195b3fdf3f863833121ea0b8e1e24645681fa
Willames Santos <willamessantos@live.com>
2024-11-08 10:55:57 -0300

    create jwt middleware

--- api/src/http/controllers/authenticate-controller.ts
+++ api/src/http/controllers/authenticate-controller.ts
@@ -19,10 +19,21 @@ export async function authenticate(
     const usersRepository = new PrismaUsersRepository()
     const authenticateUseCase = new AuthenticateUseCase(usersRepository)
 
-    await authenticateUseCase.execute({
+    const { user } = await authenticateUseCase.execute({
       email,
       password,
     })
+
+    const token = await reply.jwtSign(
+      {},
+      {
+        sign: {
+          sub: user.id,
+        },
+      },
+    )
+
+    return reply.status(200).send({ token })
   } catch (error) {
     if (error instanceof InvalidCredentialsError) {
       return reply.status(400).send({
@@ -31,6 +42,4 @@ export async function authenticate(
     }
     throw error
   }
-
-  return reply.status(200).send()
 }
--- api/src/http/routes.ts
+++ api/src/http/routes.ts
@@ -5,4 +5,6 @@ import { authenticate } from './controllers/authenticate-controller'
 export async function appRoutes(app: FastifyInstance) {
   app.post('/users', register)
   app.post('/sessions', authenticate)
+
+  /* Authenticated */
 }
